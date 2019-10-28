578698c38b97f3a0d1f0d04cbace8199ad0564e0
Damie <damienmarble@gmail.com>
2019-10-27 20:17:53 -0400

    Explode downloaded plugins.

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,17 +2,19 @@ import {AxiosInstance} from "axios";
 import PluginDescription from "./PluginDescription";
 import * as _ from "lodash";
 import path from "path";
-import fs from "fs";
+import fs, {mkdirSync} from "fs";
+import StreamZip from "node-stream-zip";
+import {app} from "electron";
 
 export default class Plugins {
-    static CACHE_DIRTY_SECONDS:number = 1 * 60 * 60 * 1000;
+    static CACHE_DIRTY_SECONDS: number = 1 * 60 * 60 * 1000;
     pluginDir: string;
-    downloadClient:AxiosInstance;
-    defaultRemoteUrl:string;
-    localPluginDescriptionCache:Map<PluginDescription, String>;
-    remotePluginDescriptionCache:Set<PluginDescription>;
+    downloadClient: AxiosInstance;
+    defaultRemoteUrl: string;
+    localPluginDescriptionCache: Map<PluginDescription, String>;
+    remotePluginDescriptionCache: Set<PluginDescription>;
 
-    constructor(pluginDir: string, downloadClient: AxiosInstance, defaultRemoteUrl:string) {
+    constructor(pluginDir: string, downloadClient: AxiosInstance, defaultRemoteUrl: string) {
         this.pluginDir = pluginDir;
         this.downloadClient = downloadClient;
         this.defaultRemoteUrl = defaultRemoteUrl;
@@ -23,18 +25,18 @@ export default class Plugins {
     /**
      * Returns the plugin description for all locally stored plugin archives.
      */
-    async getLocalPluginDescriptions(): Promise<Array<any>>{
+    async getLocalPluginDescriptions(): Promise<Array<any>> {
         return [];
     }
 
     /**
      * Returns the plugin description for all remote plugin archives.
      */
-    async getRemotePluginDescriptions(): Promise<Array<any>>{
+    async getRemotePluginDescriptions(): Promise<Array<any>> {
         try {
             const lastUpdated = (this.remotePluginDescriptionCache as any).lastUpdateTime;
             if (!lastUpdated || (lastUpdated + Plugins.CACHE_DIRTY_SECONDS) < Date.now()) {
-                const remote:Array<any> = (await (this.downloadClient.get(this.defaultRemoteUrl) as any)).data as Array<any>;
+                const remote: Array<any> = (await (this.downloadClient.get(this.defaultRemoteUrl) as any)).data as Array<any>;
                 remote.forEach(remotePlugin => {
                     this.remotePluginDescriptionCache.add(remotePlugin);
                 });
@@ -54,19 +56,26 @@ export default class Plugins {
     async updateLocalPluginCache(): Promise<Array<any>> {
         console.info("Refreshing plugin cache from remote at " + this.defaultRemoteUrl);
         const remotePlugins = await this.getRemotePluginDescriptions();
-        const differenceBetweenRemoteAndLocal:Array<PluginDescription> = _.difference(remotePlugins, Array.from(await this.localPluginDescriptionCache.values()));
-        if(differenceBetweenRemoteAndLocal.length) {
+        const differenceBetweenRemoteAndLocal: Array<PluginDescription> = _.difference(remotePlugins, Array.from(await this.localPluginDescriptionCache.values()));
+        if (differenceBetweenRemoteAndLocal.length) {
             differenceBetweenRemoteAndLocal.forEach(async plugin => {
                 try {
                     const author = plugin.author;
                     const system = plugin.system;
                     const version = plugin.version;
                     console.info(`Downloading remote plugin ${author} ${system} ${version}`);
-                    const response = await this.downloadClient.get(this.defaultRemoteUrl + `${author}/${system}/${version}/`);
-                    const targetFileName = path.resolve(this.pluginDir, `${author}-${system}-${version}.jar`);
-                    fs.writeFile(targetFileName, response.data, err =>{
-                        if(err) console.error(err);
-                        console.info(`Download ${author} ${system} ${version} finished`);
+                    const response = await this.downloadClient.get(this.defaultRemoteUrl + `${author}/${system}/${version}/`, {
+                        responseType: 'arraybuffer'
+                    });
+                    const targetFileName = path.resolve(this.pluginDir, encodeURIComponent(`${author}-${system}-${version}.jar`));
+                    const dataBuffer = Buffer.from(response.data);
+                    fs.writeFile(targetFileName, dataBuffer, err => {
+                        if (err) {
+                            console.error(err)
+                        } else {
+                            console.info(`Download ${author} ${system} ${version} finished`);
+                            this.unpackArchive(new PluginDescription(author, system, version));
+                        }
                     });
                 } catch (e) {
                     console.error("Failed to refresh plugin cache", e);
@@ -74,5 +83,40 @@ export default class Plugins {
             })
         }
         return differenceBetweenRemoteAndLocal;
+    }
+
+    private async unpackArchive(pluginId: PluginDescription): Promise<any> {
+        const encodedArchiveId = encodeURIComponent(`${pluginId.author}-${pluginId.system}-${pluginId.version}`);
+        const archivePath = path.resolve(path.join(this.pluginDir, `${encodedArchiveId}.jar`));
+        console.log(`Unpacking archive for ${pluginId.author}-${pluginId.system}-${pluginId.version} at ${archivePath}`);
+        const appDataDir = path.join(app.getPath("appData"), "neone");
+        return new Promise((resolve, reject) => {
+            fs.mkdir(appDataDir, '0777', (err) => {
+                const archive = new StreamZip({
+                    file: archivePath,
+                    storeEntries: true
+                });
+                archive.on('ready', function(){
+                    const unpackDir = path.join(appDataDir, encodedArchiveId);
+                    console.log(`Begin unpack to ${unpackDir} ${archive.entriesCount}`);
+                    fs.mkdir(unpackDir, function (err) {
+                        // If the directory already exists, we don't care, that's normal.
+                        if (err && err.code !== 'EEXIST') {
+                            return reject(err);
+                        }
+                        archive.extract(null, unpackDir,
+                            (err: any, extractedCount: number) => {
+                                if (err) {
+                                    return reject(err);
+                                }
+                                archive.close();
+                                console.log(`Unpack complete: ${extractedCount} entries`);
+                                resolve();
+                            });
+                    });
+                });
+
+            })
+        });
     }
 }
