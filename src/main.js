eb01d1684cd10042ab111c85bf4f5ee5adcb658f
Damie <damienmarble@gmail.com>
2019-10-28 22:09:25 -0400

    Implement creating and saving characters.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
-const { app, BrowserWindow, ipcMain, protocol } = require('electron');
+const {app, BrowserWindow, ipcMain, protocol} = require('electron');
 const path = require("path");
 const loadConfig = require("./loadConfig");
 const config = loadConfig(process.env.NODE_ENV);
 const axios = require("axios");
 const Plugins = require("./plugins").default;
 const url = require("url");
+const Store = require("electron-store");
+const {ObjectId} = require("mongodb");
+const store = new Store();
+store.set("characters", {});
 
 const plugins = new Plugins('.', axios, config.api.url + config.api.pluginPath);
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+    app.quit();
 }
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -18,76 +22,90 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 let mainWindow;
 
 const createWindow = () => {
-  let activePlugin;
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences:{
-      preload: path.join(__dirname, "index.js")
-    }
-  });
+    let activePlugin;
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "index.js")
+        }
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/../static/index.html`);
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/../static/index.html`);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
 
-  console.log("Create window");
+    console.log("Create window");
 
-  ipcMain.on("rendered", () => {
-    console.log("Rendered");
-  });
+    ipcMain.on("rendered", () => {
+        console.log("Rendered");
+    });
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-  ipcMain.on("plugin-list", async () => {
-    let remotePlugins = await plugins.getRemotePluginDescriptions();
-    mainWindow.webContents.send("plugin-list", remotePlugins);
-  });
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+    ipcMain.on("plugin-list", async () => {
+        let remotePlugins = await plugins.getRemotePluginDescriptions();
+        mainWindow.webContents.send("plugin-list", remotePlugins);
+    });
 
-  ipcMain.on("load-plugin", async (event, plugin) => {
-    activePlugin = plugin;
-    mainWindow.loadURL(path.join(__dirname, "..", "static", "templates", "plugin-character-page.html"));
-  });
-  ipcMain.on("get-plugin-info", async () => {
-    const pluginConfiguration = await plugins.getPluginConfiguration(activePlugin);
-    const rawPath = await plugins.getPluginResource(activePlugin, pluginConfiguration.resources.character);
-    const pluginCharacterTemplateFileName = url.pathToFileURL(rawPath);
-    mainWindow.webContents.send("get-plugin-info", activePlugin,
-        pluginCharacterTemplateFileName.toString()
-    );
-  });
+    ipcMain.on("load-plugin", async (event, plugin) => {
+        activePlugin = plugin;
+        mainWindow.loadURL(path.join(__dirname, "..", "static", "templates", "plugin-character-page.html"));
+    });
+    ipcMain.on("get-plugin-info", async () => {
+        const pluginConfiguration = await plugins.getPluginConfiguration(activePlugin);
+        const rawPath = await plugins.getPluginResource(activePlugin, pluginConfiguration.resources.character);
+        const pluginCharacterTemplateFileName = url.pathToFileURL(rawPath);
+        mainWindow.webContents.send("get-plugin-info", activePlugin,
+            pluginCharacterTemplateFileName.toString()
+        );
+    });
+
+    ipcMain.on("create-new-character", async () => {
+        mainWindow.webContents.reload();
+    });
+
+    ipcMain.on("save-character", async (e, characterData) => {
+        const existingCharacters = store.get("characters");
+        console.log(characterData.id ? "data has existing id" : "data has no existing id");
+        let id = characterData.id || new ObjectId();
+        characterData.id = id.toString();
+        existingCharacters[id] = characterData;
+        store.set("character", existingCharacters);
+        mainWindow.webContents.send("character-saved", JSON.stringify(characterData));
+    });
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', () =>{
-  createWindow();
+app.on('ready', () => {
+    createWindow();
 });
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
 // In this file you can include the rest of your app's specific main process
--- static/js/character-page.js
+++ static/js/character-page.js
@@ -1,20 +1,40 @@
-function newCharacter(){
+function getContentIframeWindow() {
     const contentIframe = $("#content");
-    const contentDocument = contentIframe[0].contentDocument;
-    contentDocument.window;
+    const contentDocument = contentIframe[0].contentWindow;
+    return contentDocument;
 }
 
-function loadCharacter(){
-
+function newCharacter() {
+    window.bridge.send("create-new-character");
 }
 
-$().ready(function(){
+function loadCharacter() {
+    const contentIframe = $("#content");
+}
+
+function saveCharacter() {
+    getContentIframeWindow().postMessage({action: "get-character"});
+}
+
+$().ready(function () {
     let loadedPluginInfo;
-    window.bridge.on("get-plugin-info", function(e, pluginInfo, iframeUrl){
+    window.bridge.on("get-plugin-info", function (e, pluginInfo, iframeUrl) {
         loadedPluginInfo = pluginInfo;
         $("#content").attr("src", iframeUrl);
     });
-    window.bridge.send("get-plugin-info", );
+    window.bridge.send("get-plugin-info");
+    window.addEventListener("message", function (event) {
+        console.log(event.data);
+        window.bridge.send("save-character", event.data);
+    });
     $("#new-character").on("click", newCharacter);
     $("#load-character").on("click", loadCharacter);
+    $("#save-character").on("click", saveCharacter);
+
+    window.bridge.on("character-saved", function (e, postSaveCharacter) {
+        getContentIframeWindow().postMessage({
+            action: "character-saved",
+            character: postSaveCharacter
+        });
+    });
 });
