617731985e393e00d61e5d0fd74daca18b2efc46
Damie <damienmarble@gmail.com>
2019-10-28 20:57:15 -0400

    Implement loading plugin list in index.js instead of preload script.

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,20 @@
-window.index = function(){
-	var spinner = $("#spinner");
-	window.bridge.send("rendered");
-};
-$(document).ready(window.index);
+function hideList() {
+    availablePlugins.hide();
+}
+window.bridge.on("plugin-list", (event, list) => {
+    const availablePlugins = $("#available-plugins");
+    list.forEach(ap => {
+        console.log(ap);
+        const newElement = $("<li>");
+        newElement.text(ap.system + " " + ap.version);
+        newElement.on("click", function () {
+            const pluginToLoad = ap;
+            window.bridge.send("load-plugin", pluginToLoad);
+        });
+        availablePlugins.append(newElement);
+    });
+    $("#spinner").hide();
+});
+$(document).ready(function(){
+    window.bridge.send("plugin-list");
+});
