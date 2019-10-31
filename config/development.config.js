2cbf94ba5e2e7db72aac56b2465faf94ebb2a323
Damie <damienmarble@gmail.com>
2019-10-30 20:15:25 -0400

    Remove listeners on duplication.

--- config/development.config.js
+++ config/development.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    features: {
+        CLOUD_FEATURES: true
+    },
     api: {
         url: 'http://localhost:8080',
         pluginPath: '/plugins/',
@@ -6,10 +9,13 @@ module.exports = {
             google: "/login/google"
         }
     },
-    auth: {
-        google: {
-            clientId: "275558456725-6jus1cc4jisj4t1md64oog6q47den271.apps.googleusercontent.com",
-            clientSecret: "FiF_ZG4erQZwJZQfjRQ-zXRK"
-        }
+    services: {
+        auth: {
+            google: {
+                clientId: "275558456725-6jus1cc4jisj4t1md64oog6q47den271.apps.googleusercontent.com",
+                clientSecret: "FiF_ZG4erQZwJZQfjRQ-zXRK",
+                apiUrl: "https://people.googleapis.com/v1/people/me"
+            }
+        },
     }
 };
